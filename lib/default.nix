da20cf3e06bb0e3416f4ad3aa016f64e21ca78f7
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2024-05-30 08:16:33 -0400

    Tweak eachSystemOverlay definition

--- lib/default.nix
+++ lib/default.nix
@@ -17,13 +17,9 @@ in rec {
     );
 
   eachSystemOverlay = overlay: f:
-    genAttrs systems (
-      system:
-        f {
-          system = system;
-          pkgs = nixpkgs.legacyPackages.${system}.extend overlay;
-        }
-    );
+    eachSystem ({ pkgs, ...} @ inputs: f (inputs // {
+      pkgs = pkgs.extend overlay;
+    }));
 
   # Map over attribute names (only)
   # dict[str, a] -> (str -> str) -> dict[str, a]
