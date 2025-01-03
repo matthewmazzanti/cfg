aa1debc5fc73442b4e0f235e3338e2eae46f7dae
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2025-01-03 08:15:31 -0500

    Fix with a file ref

--- sys/home-assistant/home-automation.nix
+++ sys/home-assistant/home-automation.nix
@@ -1,6 +1,6 @@
 { config, ... }:
 let
-  images = builtins.fromJSON "images.lock";
+  images = builtins.fromJSON ./images.lock;
 in {
   # Homeassistant stuff
   networking.firewall.allowedTCPPorts = [ 8123 8091 ];
