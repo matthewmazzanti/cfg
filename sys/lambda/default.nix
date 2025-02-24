7850f4807f2fe809b5d5f2e8d88ac0646568906e
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2025-02-24 08:38:30 -0500

    Unpack inline modules

--- sys/lambda/default.nix
+++ sys/lambda/default.nix
@@ -3,158 +3,7 @@ with lib;
 let
   keys = (import ../../old/modules/keys);
 in {
-  imports = [
-    ./hardware.nix
-    # modules/default.nix
-    {
-      time.timeZone = "America/New_York";
-      i18n.defaultLocale = "en_US.UTF-8";
-
-      console = {
-        font = "${pkgs.terminus_font}/share/consolefonts/ter-v32n.psf.gz";
-        keyMap = "us";
-      };
-
-      environment = {
-        systemPackages = with pkgs; [
-          gnutls
-          pciutils
-          usbutils
-
-          wget
-          curl
-          tree
-          git
-
-          gnupg
-          zsh
-          tmux
-          kitty.terminfo
-          ethtool
-          custom."nvim/root"
-        ];
-      };
-
-      environment.pathsToLink = [ "/share/zsh" ];
-
-      environment.etc = {
-        "inputrc".text = ''
-          set editing-mode vi
-          set keymap vi
-        '';
-      };
-
-      programs.zsh.enable = true;
-      programs.gnupg.agent = {
-        enable = true;
-        enableSSHSupport = true;
-        pinentryPackage = pkgs.pinentry-qt;
-      };
-
-      services.openssh = {
-        enable = true;
-        settings = {
-          PermitRootLogin = "no";
-          PasswordAuthentication = false;
-          KbdInteractiveAuthentication = false;
-        };
-      };
-
-      services.getty = {
-        greetingLine = "${config.networking.hostName}";
-        helpLine = mkForce "";
-      };
-
-      security.pki.certificates = [
-        (builtins.readFile ../../old/modules/keys/ca.crt)
-      ];
-
-      users.defaultUserShell = pkgs.zsh;
-
-      users.users.mmazzanti = {
-        isNormalUser = true;
-        extraGroups = [ "wheel" ];
-        openssh.authorizedKeys.keys = with keys.mmazzanti; [
-          lambda
-          iota
-          beta
-        ];
-      };
-    }
-    # graphical.nix
-    {
-      users.users.mmazzanti.extraGroups = [ "video" "audio" ];
-
-      programs.dconf.enable = true;
-
-      services.xserver = {
-        enable = true;
-        displayManager.startx.enable = true;
-        autoRepeatDelay = 300;
-        autoRepeatInterval = 40;
-        enableCtrlAltBackspace = true;
-        videoDrivers = ["amdgpu"];
-      };
-
-      # Not sure what this is for
-      services.dbus.enable = true;
-
-      # 24.09 made pipewire the default - unset for now
-      services.pipewire.enable = false;
-      services.pulseaudio = {
-        enable = true;
-        support32Bit = true;
-        daemon.config = {
-          resample-method = "speex-float-10";
-          avoid-resampling = "true";
-          default-sample-rate = "48000";
-        };
-      };
-
-      hardware.graphics = {
-        # Enables gpu accelerated graphics
-        enable = true;
-        # Not sure what this is for
-        extraPackages = [ pkgs.libva ];
-      };
-    }
-  ];
-
-  nixpkgs.config.allowUnfree = true;
-
-  services = {
-    xserver.dpi = 168;
-    udev = {
-      packages = [ pkgs.dolphin-emu ];
-      extraHwdb = ''
-        # Naga Trinity
-        mouse:usb:v1532p0067:*
-            MOUSE_WHEEL_CLICK_ANGLE=30
-      '';
-      extraRules = ''
-        # Atmel ATMega32U4
-        SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2ff4", MODE:="0666"
-        # Atmel USBKEY AT90USB1287
-        SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2ffb", MODE:="0666"
-        # Atmel ATMega32U2
-        SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2ff0", MODE:="0666"
-        # SteelSeries Rival 500
-        SUBSYSTEMS=="usb", ATTRS{idVendor}=="1038", ATTRS{idProduct}=="170e", MODE:="0666", OWNER="1000"
-        # Kyria Keyboard
-        SUBSYSTEMS=="usb", ATTRS{idVendor}=="feed", ATTRS{idProduct}=="0000", MODE:="0666", OWNER="1000"
-        # Naga Trinity
-        SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0067", MODE:="0666", OWNER="1000"
-      '';
-    };
-  };
-
-  environment.systemPackages = with pkgs; [
-    ddcutil
-    radeontop
-    radeon-profile
-  ];
-
-  programs.openvpn3.enable = true;
+  imports = [ ./hardware.nix ];
 
   boot.supportedFilesystems = [ "zfs" ];
   boot.binfmt.emulatedSystems = ["aarch64-linux"];
@@ -165,17 +14,170 @@ in {
     efi.canTouchEfiVariables = true;
   };
 
+  time.timeZone = "America/New_York";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Console visual things
+  console = {
+    font = "${pkgs.terminus_font}/share/consolefonts/ter-v32n.psf.gz";
+    keyMap = "us";
+  };
+  services.getty = {
+    greetingLine = "${config.networking.hostName}";
+    helpLine = mkForce "";
+  };
+
+  # Enable and set zsh as default
+  programs.zsh.enable = true;
+  # Not sure what this is for
+  environment.pathsToLink = [ "/share/zsh" ];
+  users.defaultUserShell = pkgs.zsh;
+
+  # Set vi mode in global inputrc
+  environment.etc = {
+    "inputrc".text = ''
+      set editing-mode vi
+      set keymap vi
+    '';
+  };
+
+  # Hardware config
+  services.udev = {
+    extraHwdb = ''
+      # Naga Trinity
+      mouse:usb:v1532p0067:*
+          MOUSE_WHEEL_CLICK_ANGLE=30
+    '';
+    extraRules = ''
+      # Atmel ATMega32U4
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2ff4", MODE:="0666"
+      # Atmel USBKEY AT90USB1287
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2ffb", MODE:="0666"
+      # Atmel ATMega32U2
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2ff0", MODE:="0666"
+      # SteelSeries Rival 500
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="1038", ATTRS{idProduct}=="170e", MODE:="0666", OWNER="1000"
+      # Kyria Keyboard
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="feed", ATTRS{idProduct}=="0000", MODE:="0666", OWNER="1000"
+      # Naga Trinity
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0067", MODE:="0666", OWNER="1000"
+    '';
+  };
+
+  # === Graphics Settings ===
+  hardware.graphics = {
+    # Enables gpu accelerated graphics
+    enable = true;
+    # Not sure what this is for
+    extraPackages = [ pkgs.libva ];
+  };
+  # xserver settings - these are overriden in custom BS happening in home
+  # manager
+  services.xserver = {
+    enable = true;
+    dpi = 168;
+    displayManager.startx.enable = true;
+    autoRepeatDelay = 300;
+    autoRepeatInterval = 40;
+    enableCtrlAltBackspace = true;
+    videoDrivers = ["amdgpu"];
+  };
+
+  # === AUDIO SETTINGS ===
+  # 24.09 made pipewire the default - unset for now
+  services.pipewire.enable = false;
+  services.pulseaudio = {
+    enable = true;
+    support32Bit = true;
+    daemon.config = {
+      resample-method = "speex-float-10";
+      avoid-resampling = "true";
+      default-sample-rate = "48000";
+    };
+  };
+
   networking = {
     hostId = "f43d79c6";
     hostName = "lambda";
-    interfaces.enp5s0.wakeOnLan.enable = true;
-    firewall.allowedTCPPorts = [ 8080 ];
   };
 
+  # Enable wake on lan
+  networking.interfaces.enp5s0.wakeOnLan.enable = true;
+
   users.users.mmazzanti.extraGroups = [
+    "wheel"
     "usb"
     "dialout"
     "input"
+    "video"
+    "audio"
+  ];
+  users.users.mmazzanti.isNormalUser = true;
+
+
+  # gnupg - TODO: Remove
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+    pinentryPackage = pkgs.pinentry-qt;
+  };
+
+  # SSH
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+    };
+  };
+  users.users.mmazzanti.openssh.authorizedKeys.keys = with keys.mmazzanti; [
+    lambda
+    iota
+    beta
+  ];
+
+  # PKI - trust ca root.
+  # TODO: Make new cert chain
+  security.pki.certificates = [
+    (builtins.readFile ../../old/modules/keys/ca.crt)
+  ];
+
+
+  # Gaming things
+  nixpkgs.config.allowUnfree = true;
+  programs.steam.enable = true;
+  services.udev.packages = [ pkgs.dolphin-emu ];
+
+  # Not sure what this is for
+  programs.dconf.enable = true;
+  services.dbus.enable = true;
+
+
+  # === PACKAGES ===
+  environment.systemPackages = with pkgs; [
+    # Debugging utilities
+    pciutils
+    usbutils
+    radeontop
+    radeon-profile
+    ddcutil
+
+    # Basic console utils
+    wget
+    curl
+    tree
+    git
+
+    # Not sure what this is for
+    gnutls
+    # gnupg
+    gnupg
+    zsh
+    tmux
+    kitty.terminfo
+    ethtool
+    custom."nvim/root"
   ];
 
   home-manager.users.mmazzanti = {
@@ -195,8 +197,6 @@ in {
       '';
     };
   };
-
-  programs.steam.enable = true;
 
   system.stateVersion = "20.09";
 }
