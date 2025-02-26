2b9ec713814d8d02ca20f051d4c6583e97561ef0
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2025-02-26 08:51:54 -0500

    Add nginx config

--- sys/home-assistant/home-automation.nix
+++ sys/home-assistant/home-automation.nix
@@ -44,4 +44,21 @@ in {
       };
     };
   };
+
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    # recommendedTlsSettings = true; 
+    virtualHosts."home-assistant.iot" = {
+      # forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8123";
+        proxyWebsockets = true;
+        extraConfig =
+          # required when the server wants to use HTTP Authentication
+          "proxy_pass_header Authorization;"
+          ;
+      };
+    };
+  };
 }
