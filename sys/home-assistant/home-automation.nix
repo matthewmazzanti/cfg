23fc56f676dd2e0db61ec923ca59cc0e9bf77afd
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2025-02-26 10:13:18 -0500

    Remove extra config

--- sys/home-assistant/home-automation.nix
+++ sys/home-assistant/home-automation.nix
@@ -47,19 +47,13 @@ in {
 
   services.nginx = {
     enable = true;
-    # recommendedProxySettings = true;
+    recommendedProxySettings = true;
     # recommendedTlsSettings = true;
     virtualHosts."home-assistant.iot" = {
       # forceSSL = true;
       locations."/" = {
         proxyPass = "http://127.0.0.1:8123";
-        # proxyWebsockets = true;
-        # extraConfig =
-        #   # required when the target is also TLS server with multiple hosts
-        #   "proxy_ssl_server_name on;" +
-        #   # required when the server wants to use HTTP Authentication
-        #   "proxy_pass_header Authorization;"
-        #   ;
+        proxyWebsockets = true;
       };
     };
   };
