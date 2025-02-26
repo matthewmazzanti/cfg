6d99161503f873db3e852ea28bec15025d78ab64
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2025-02-26 10:14:54 -0500

    Try removing websocket proxy

--- sys/home-assistant/home-automation.nix
+++ sys/home-assistant/home-automation.nix
@@ -53,7 +53,7 @@ in {
       # forceSSL = true;
       locations."/" = {
         proxyPass = "http://127.0.0.1:8123";
-        proxyWebsockets = true;
+        # proxyWebsockets = true;
       };
     };
   };
