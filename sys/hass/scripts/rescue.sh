22f3cbfe761c824ac6a521f2a82c24f04a1c61b3
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2025-05-25 17:47:59 -0400

    Change device id

--- sys/hass/scripts/rescue.sh
+++ sys/hass/scripts/rescue.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -xeu
 
-KEYDEV="/dev/disk/by-path/pci-0000:00:14.0-usb-0:1:1.0-scsi-0:0:0:0"
+KEYDEV="/dev/disk/by-id/usb-USB_SanDisk_3.2Gen1_010120f1fc6b4bb4ab4d7391d2fdf545bb3e6e6143450208f305b9fd806943b3e4e900000000000000000000f833a26f001c4900835581072a33742e-0:0"
 ROOTDEV="/dev/disk/by-path/pci-0000:02:00.0-nvme-1"
 part="$ROOTDEV-part/by-partlabel"
 
