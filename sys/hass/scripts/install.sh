86edf76da24948bb9005f83b2303599c08bee890
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2025-05-25 10:18:24 -0400

    Add another mkdir

--- sys/hass/scripts/install.sh
+++ sys/hass/scripts/install.sh
@@ -7,6 +7,6 @@ if ! command -v git; then
     nix-env -iA nixpkgs.git
 fi
 
-mkdir ~/src/nix
+mkdir --parents ~/src/nix
 git clone https://github.com/matthewmazzanti/cfg.git ~/src/nix/cfg
 nixos-install --root /mnt --flake "~/src/nix/cfg#hass"
