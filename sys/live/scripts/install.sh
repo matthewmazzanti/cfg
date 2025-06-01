d0c43eb98b5edae3c3ef9f7efe117e674dfce7d9
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2025-06-01 10:09:10 -0400

    Use SUDO_HOME instead of home

--- sys/live/scripts/install.sh
+++ sys/live/scripts/install.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-if ! command -v git; then nix-env -iA nixpkgs.git; fi
-
-if ! [ -e "$HOME/src/nix" ]; then
-    mkdir -p "$HOME/src/nix"
-    git clone https://github.com/matthewmazzanti/cfg.git "$HOME/src/nix/cfg"
-else
-    git -C "$HOME/src/nix/cfg" pull
-fi
-
 # Install nixos
 nixos-install \
     --root /mnt \
     --no-channel-copy \
     --no-root-password \
-    --flake "$HOME/src/nix/cfg#live"
+    --flake "$SUDO_HOME/src/nix/cfg#live"
