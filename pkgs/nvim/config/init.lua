48a0c861d6e277d693f72e0e719fc9fc19b27416
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2025-06-13 23:00:43 -0400

    Add new lua loader

--- pkgs/nvim/config/init.lua
+++ pkgs/nvim/config/init.lua
@@ -1,5 +1,8 @@
 -- GENERAL CONFIG --
 
+-- Enable faster lua loader - saves ~20ms!
+vim.loader.enable()
+
 -- Show numbers on each line next to text. Relative numbers for jumps
 -- relativenumber may be a bit heavy on low-power systems
 vim.opt.number = true
