c34b70f09b9b897501b8e7a1818ae77a61be31c7
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2025-06-25 11:49:18 -0400

    Not sure why the condition was triggering

--- lib.sh
+++ lib.sh
@@ -35,9 +35,6 @@ wipe_root_part() {
 
     # Wipe all signatures on current partitions
     for part in "$dev"?*; do
-        if [ ! -e "$part" ]; then
-            continue
-        fi
         wipefs --all "$part"
     done
 
