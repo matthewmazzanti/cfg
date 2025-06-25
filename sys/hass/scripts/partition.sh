3d43bf3f576361de5867d6e8894423ff170173a4
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2025-06-25 13:50:12 -0400

    Allow keydev to exist

--- sys/hass/scripts/partition.sh
+++ sys/hass/scripts/partition.sh
@@ -60,7 +60,7 @@ cryptsetup open \
 
 # Create Keyfile
 mkfs.ext4 -L key -U "$KEY_FS" "$KEY_DEV"
-mkdir /key-dev
+mkdir -p /key-dev
 mount -t ext4 -o noatime,nodiratime "$by_uuid/$KEY_FS" /key-dev
 echo "hass" > /key-dev/system
 chmod 400 /key-dev/system
