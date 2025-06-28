214a9b78d388dc1e9d9dbe0a2538476172cf3234
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2025-06-27 20:52:35 -0400

    Add watch to beta

--- sys/beta/default.nix
+++ sys/beta/default.nix
@@ -70,7 +70,7 @@ in {
   users.users.mmazzanti.packages =
     (with pkgs; [
       # Terminal utilities
-      bat fd fzf git ripgrep tree jq yq visidata htop
+      bat fd fzf git ripgrep tree jq yq visidata htop unixtools.watch
       # Networking
       nmap httpie wget curl
       # Languages
