b92ec87eb4be1276f250918e4a525ddff1effe58
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2025-06-28 11:56:19 -0400

    Fix unbound var

--- sys/framework/scripts/partition.sh
+++ sys/framework/scripts/partition.sh
@@ -13,7 +13,7 @@ ROOT_CRYPT="0050d616-0fd0-40da-8760-e14cc7f108f6"
 ROOT_FS="1e7cc615-eaa3-4636-be79-c67bb165cfd0"
 BLOCK_SIZE="4096"
 
-confirm_reformat "$DEV" "$KEY_DEV"
+confirm_reformat "$DEV"
 
 passfile="$(get_passfile)"
 trap 'rm -f "$passfile"' EXIT INT TERM
