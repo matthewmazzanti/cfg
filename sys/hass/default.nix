002ead944ae3d97b7ca51fa1b8416cc9654ab4bc
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2025-07-02 00:09:59 -0400

    Remove most bridge stuff

--- sys/hass/default.nix
+++ sys/hass/default.nix
@@ -41,83 +41,24 @@
       };
     };
 
-    netdevs."05-enp1s0.18" = {
-      netdevConfig = {
-        Name = "enp1s0.18";
-        Kind = "vlan";
-      };
-      vlanConfig = {
-        Id = 18;
-      };
-    };
-
-    networks."05-enp1s0.18" = {
-      matchConfig.Name = "enp1s0.18";
-      networkConfig = {
-        # Disable networking through this interface for the host
-        DHCP = "no";
-        IPv6AcceptRA = false;
-        LinkLocalAddressing = "no";
-        MACVLAN = ["macvlan-ha"];
-      };
-    };
-
-    netdevs."10-br-int" = {
-      netdevConfig = {
-        Name = "br-int";
-        Kind = "bridge";
-      };
-    };
-
-    networks."10-br-int" = {
-      matchConfig.Name = "br-int";
-      networkConfig.Address = "192.168.0.1/24";
-    };
-
-    netdevs."20-veth-ha" = {
-      netdevConfig = {
-        Name = "veth-ha";
-        Kind = "veth";
-      };
-      peerConfig.Name = "veth-ha-c";
-    };
-
-    networks."20-veth-ha" = {
-      matchConfig.Name = "veth-ha";
-      networkConfig.Bridge = "br-int";
-    };
-
-    netdevs."20-veth-zwave" = {
-      netdevConfig = {
-        Name = "veth-zwave";
-        Kind = "veth";
-      };
-      peerConfig.Name = "veth-zwave-c";
-    };
-
-    networks."20-veth-zwave" = {
-      matchConfig.Name = "veth-zwave";
-      networkConfig.Bridge = "br-int";
-    };
-
-    netdevs."30-macvlan-ha" = {
-      netdevConfig = {
-        Name = "macvlan-ha";
-        Kind = "macvlan";
-      };
-      macvlanConfig = {
-        Mode = "bridge";
-      };
-    };
-
-    networks."30-macvlan-ha" = {
-      matchConfig.Name = "macvlan-ha";
-      networkConfig = {
-        Address = "172.18.2.10/20";
-        Gateway = "172.18.0.1";
-        DNS = [ "172.18.0.1" ];
-        ConfigureWithoutCarrier = true;
-      };
-    };
+    # netdevs."05-enp1s0.18" = {
+    #   netdevConfig = {
+    #     Name = "enp1s0.18";
+    #     Kind = "vlan";
+    #   };
+    #   vlanConfig = {
+    #     Id = 18;
+    #   };
+    # };
+    #
+    # networks."05-enp1s0.18" = {
+    #   matchConfig.Name = "enp1s0.18";
+    #   networkConfig = {
+    #     # Disable networking through this interface for the host
+    #     DHCP = "no";
+    #     IPv6AcceptRA = false;
+    #     LinkLocalAddressing = "no";
+    #   };
+    # };
   };
 }
