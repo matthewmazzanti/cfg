ba2391267c8ef7eaf31a59981d0cb5122939ca15
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2025-07-03 23:34:31 -0400

    Remove ha-internal for test

--- sys/hass/default.nix
+++ sys/hass/default.nix
@@ -78,13 +78,13 @@
         name = "hass";
         networks = [
           "ha-macvlan:mac=02:11:22:33:44:55"
-          "ha-internal:alias=hass"
+          # "ha-internal:alias=hass"
         ];
         addCapabilities = [
           "NET_RAW"
         ];
         image = "docker.io/nicolaka/netshoot:latest";
-        dns = [ "172.18.0.1" ];
+        # dns = [ "172.18.0.1" ];
         entrypoint = builtins.toJSON ["sleep" "infinity"];
       };
     };
