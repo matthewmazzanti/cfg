143140ac80a2d1dfbea63f7baea8d9dd8ec647c6
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2025-07-03 23:42:15 -0400

    Fix net

--- sys/hass/default.nix
+++ sys/hass/default.nix
@@ -75,8 +75,8 @@
       hass.containerConfig = {
         name = "hass";
         networks = [
-          # "ha-macvlan:mac=02:11:22:33:44:55"
-          "ha-internal:alias=hass,ip=172.18.2.10"
+          "ha-macvlan:ip=172.18.2.10,mac=02:11:22:33:44:55"
+          # "ha-internal:alias=hass"
         ];
         addCapabilities = [
           "NET_RAW"
