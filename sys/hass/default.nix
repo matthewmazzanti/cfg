3eed445c8c6dcbcede5c8cfe66f2804737d9047c
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2025-07-05 10:07:03 -0400

    Switch back to bridge/port forward

--- sys/hass/default.nix
+++ sys/hass/default.nix
@@ -61,8 +61,8 @@
       ha-bridge = {
         networkConfig = {
           driver = "bridge";
+          internal = true;
           subnets = [ "192.168.1.0/24" ];
-          gateways = [ "192.168.1.1" ];
         };
       };
       ha-macvlan = {
@@ -79,23 +79,6 @@
       };
     };
 
-    pods.ha.podConfig = {
-      name = "ha";
-      networks = [
-        "ha-bridge:ip=192.168.1.2"
-        "ha-macvlan:ip=172.18.2.10,mac=02:fd:38:25:58:f9"
-      ];
-      uidMaps = ["0:200000:65536"];
-      gidMaps = ["0:200000:65536"];
-      dns = [ "172.18.0.1" ];
-      publishPorts = [
-        "172.16.2.10:80:8080"
-        "172.16.2.10:443:8443"
-        "127.0.0.1:80:8080"
-        "127.0.0.1:443:8443"
-      ];
-    };
-
     containers = {
       nginx = {
         unitConfig = {
@@ -104,12 +87,20 @@
         };
         containerConfig = {
           name = "nginx";
-          pod = "ha.pod";
           image = "docker.io/library/nginx:alpine-slim@sha256:e4e764cb35f666f44dd4e1da4291a5f73bb8bff2a9464ccecd8a05a2b7226ad5";
+          uidMaps = ["0:200000:65536"];
+          gidMaps = ["0:200000:65536"];
           # dropCapabilities = ["ALL"];
           # noNewPrivileges = true;
           # readOnly = true;
           # tmpfses = [ "/var/run" "/tmp" ];
+          networks = [ "ha-bridge:ip=192.168.1.2" ];
+          publishPorts = [
+            "172.16.2.10:80:8080"
+            "172.16.2.10:443:8443"
+            "127.0.0.1:80:8080"
+            "127.0.0.1:443:8443"
+          ];
           volumes = [
             "${./nginx.conf}:/etc/nginx/nginx.conf:ro"
             "/var/lib/nginx/ssl:/etc/nginx/ssl:ro"
@@ -127,11 +118,18 @@
           name = "hass";
           pod = "ha.pod";
           image = "ghcr.io/home-assistant/home-assistant:stable@sha256:d80b831e5a7ec80949231d45c4bea9102c60d5e2f02c961d3120e5d48226cbc9";
+          uidMaps = ["0:200000:65536"];
+          gidMaps = ["0:200000:65536"];
           # dropCapabilities = ["ALL"];
           addCapabilities = ["FOWNER" "NET_RAW"];
           # noNewPrivileges = true;
           # readOnly = true;
           # tmpfses = [ "/var/run" "/tmp" ];
+          networks = [
+            "ha-bridge:ip=192.168.1.3"
+            "ha-macvlan:ip=172.18.2.10,mac=02:fd:38:25:58:f9"
+          ];
+          dns = [ "172.18.0.1" ];
           volumes = [
             "/etc/localtime:/etc/localtime:ro"
             "/var/lib/hass:/config:rw"
@@ -152,11 +150,14 @@
           name = "zwave";
           pod = "ha.pod";
           image = "zwavejs/zwave-js-ui@sha256:52b6ee2c37fa1a3c13a8d8f59b45145b546ec31b5c85d5053e1279fc558c5a1e";
+          uidMaps = ["0:200000:65536"];
+          gidMaps = ["0:200000:65536"];
           # dropCapabilities = ["ALL"];
           # addCapabilities = ["FOWNER" "NET_RAW"];
           # noNewPrivileges = true;
           # readOnly = true;
           # tmpfses = [ "/var/run" "/tmp" ];
+          networks = [ "ha-bridge:ip=192.168.1.4" ];
           devices = [ "/dev/serial/by-id/usb-Silicon_Labs_CP2102N_USB_to_UART_Bridge_Controller_e015830c1ba4eb11a4f62a259da30875-if00-port0:/dev/zwave" ];
           environments.TZ = config.time.timeZone;
         };
