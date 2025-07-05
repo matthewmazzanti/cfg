a50d493eab943e194204bb3909a10a4450a836d1
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2025-07-05 08:50:40 -0400

    Fix networks

--- sys/hass/default.nix
+++ sys/hass/default.nix
@@ -72,7 +72,10 @@
 
     pods.ha.podConfig = {
       name = "ha";
-      networks = [ "ha-macvlan:ip=172.18.2.10,mac=02:fd:38:25:58:f9" ];
+      networks = [
+        "podman"
+        "ha-macvlan:ip=172.18.2.10,mac=02:fd:38:25:58:f9"
+      ];
       uidMaps = ["0:200000:65536"];
       gidMaps = ["0:200000:65536"];
       dns = [ "172.18.0.1" ];
@@ -94,9 +97,7 @@
           name = "nginx";
           pod = "ha.pod";
           image = "docker.io/library/nginx:alpine-slim@sha256:e4e764cb35f666f44dd4e1da4291a5f73bb8bff2a9464ccecd8a05a2b7226ad5";
-          # image = "docker.io/nicolaka/netshoot:latest";
-          # entrypoint = builtins.toJSON ["sleep" "infinity"];
-          # dropCapabilities = ["ALL"];
+          dropCapabilities = ["ALL"];
           # noNewPrivileges = true;
           # readOnly = true;
           # tmpfses = [ "/var/run" "/tmp" ];
@@ -117,10 +118,8 @@
           name = "hass";
           pod = "ha.pod";
           image = "ghcr.io/home-assistant/home-assistant:stable@sha256:d80b831e5a7ec80949231d45c4bea9102c60d5e2f02c961d3120e5d48226cbc9";
-          # image = "docker.io/nicolaka/netshoot:latest";
-          # entrypoint = builtins.toJSON ["sleep" "infinity"];
-          # dropCapabilities = ["ALL"];
-          # addCapabilities = ["FOWNER" "NET_RAW"];
+          dropCapabilities = ["ALL"];
+          addCapabilities = ["FOWNER" "NET_RAW"];
           # noNewPrivileges = true;
           # readOnly = true;
           # tmpfses = [ "/var/run" "/tmp" ];
