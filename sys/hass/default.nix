f7d891632dfb82306d032fa0ab78164d76aaa5c7
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2025-07-05 19:22:47 -0400

    Rollback netns

--- sys/hass/default.nix
+++ sys/hass/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, flake, ... }: {
+{ config, pkgs, flake, ... }: {
   imports = [
     flake.modules.base
     flake.modules.impermanence
@@ -22,9 +22,7 @@
     nftables
   ];
 
-  environment.persistence."/persist".directories = [
-    "/var/lib/containers"
-  ];
+  environment.persistence."/persist".directories = [ "/var/lib/containers" ];
 
   # User config
   users.users.mmazzanti = {
@@ -33,7 +31,6 @@
   };
 
   networking.firewall.enable = false;
-
   networking.useDHCP = false;
   systemd.network = {
     enable = true;
@@ -59,76 +56,48 @@
 
     networks."05-enp1s0.18" = {
       matchConfig.Name = "enp1s0.18";
-    };
-  };
-
-  systemd.services.ha-netns = {
-    description = "Create mynetns with ha-macvlan and ha-veth";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network.target" ];
-    path = with pkgs; [ bash iproute2 ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStartPre = ''-${./netns/delete.sh}'';
-      ExecStart = ./netns/create.sh;
-      ExecStop = ./netns/delete.sh;
-      RemainAfterExit = true;
-    };
-  };
-
-  boot.kernel.sysctl."net.ipv4.ip_forward" = true;
-  networking.nftables.enable = true;
-  networking.nftables.tables = {
-    nat = {
-      family = "ip";
-      chains = {
-        prerouting = {
-          type = "nat";
-          hook = "prerouting";
-          priority = -100;
-          policy = "accept";
-          rules = [
-            # HTTP forwards
-            { expr = "iif lo tcp dport 80 dnat to 192.168.1.2:8080"; }
-            { expr = "ip daddr 172.16.2.10 tcp dport 80 dnat to 192.168.1.2:8080"; }
-
-            # HTTPS forwards
-            {
-              expr = "iif lo tcp dport 443 dnat to 192.168.1.2:8443";
-            }
-            {
-              expr = "ip daddr 172.16.2.10 tcp dport 443 dnat to 192.168.1.2:8443";
-            }
-          ];
-        };
-      };
-    };
-
-    filter = {
-      family = "ip";
-      chains = {
-        forward = {
-          type = "filter";
-          hook = "forward";
-          priority = 0;
-          policy = "accept";
-          rules = [
-            # Allow HTTP and HTTPS forwarding to netns service
-            {
-              expr = "ip daddr 192.168.1.2 tcp dport 8080 accept";
-            }
-            {
-              expr = "ip daddr 192.168.1.2 tcp dport 8443 accept";
-            }
-          ];
-        };
-      };
+      # Don't bring the link up
+      linkConfig.Unmanaged = true;
     };
   };
 
   virtualisation.quadlet = {
-    autoEscape = true;
-    /*
+    networks = {
+      ha-bridge = {
+        driver = "bridge";
+        subnets = [ "192.168.1.0/24" ];
+      };
+      ha-macvlan = {
+        unitConfig = {
+          after = [ "sys-devices-virtual-net-enp1s0.18.device" ];
+          requires = [ "sys-devices-virtual-net-enp1s0.18.device" ];
+        };
+        networkConfig = {
+          driver = "macvlan";
+          options = "parent=enp1s0.18";
+          subnets = [ "172.18.0.0/20" ];
+          gateways = [ "172.18.0.1" ];
+        };
+      };
+    };
+
+    pods.ha.podConfig = {
+      name = "ha";
+      networks = [
+        "ha-bridge"
+        "ha-macvlan:ip=172.18.2.10,mac=02:fd:38:25:58:f9"
+      ];
+      uidMaps = ["0:200000:65536"];
+      gidMaps = ["0:200000:65536"];
+      dns = [ "172.18.0.1" ];
+      publishPorts = [
+        "172.16.2.10:80:8080"
+        "172.16.2.10:443:8443"
+        "127.0.0.1:80:8080"
+        "127.0.0.1:443:8443"
+      ];
+    };
+
     containers = {
       nginx = {
         unitConfig = {
@@ -137,20 +106,12 @@
         };
         containerConfig = {
           name = "nginx";
+          pod = "ha.pod";
           image = "docker.io/library/nginx:alpine-slim@sha256:e4e764cb35f666f44dd4e1da4291a5f73bb8bff2a9464ccecd8a05a2b7226ad5";
-          uidMaps = ["0:200000:65536"];
-          gidMaps = ["0:200000:65536"];
           # dropCapabilities = ["ALL"];
-          noNewPrivileges = true;
+          # noNewPrivileges = true;
           # readOnly = true;
           # tmpfses = [ "/var/run" "/tmp" ];
-          networks = [ "ha-bridge:ip=192.168.1.2" ];
-          publishPorts = [
-            "172.16.2.10:80:8080"
-            "172.16.2.10:443:8443"
-            "127.0.0.1:80:8080"
-            "127.0.0.1:443:8443"
-          ];
           volumes = [
             "${./nginx.conf}:/etc/nginx/nginx.conf:ro"
             "/var/lib/nginx/ssl:/etc/nginx/ssl:ro"
@@ -166,19 +127,13 @@
         };
         containerConfig = {
           name = "hass";
+          pod = "ha.pod";
           image = "ghcr.io/home-assistant/home-assistant:stable@sha256:d80b831e5a7ec80949231d45c4bea9102c60d5e2f02c961d3120e5d48226cbc9";
-          uidMaps = ["0:200000:65536"];
-          gidMaps = ["0:200000:65536"];
           # dropCapabilities = ["ALL"];
           addCapabilities = ["FOWNER" "NET_RAW"];
-          noNewPrivileges = true;
+          # noNewPrivileges = true;
           # readOnly = true;
           # tmpfses = [ "/var/run" "/tmp" ];
-          networks = [
-            "ha-bridge:ip=192.168.1.3"
-            "ha-macvlan:ip=172.18.2.10,mac=02:fd:38:25:58:f9"
-          ];
-          dns = [ "172.18.0.1" ];
           volumes = [
             "/etc/localtime:/etc/localtime:ro"
             "/var/lib/hass:/config:rw"
@@ -197,20 +152,17 @@
         };
         containerConfig = {
           name = "zwave";
+          pod = "ha.pod";
           image = "zwavejs/zwave-js-ui@sha256:52b6ee2c37fa1a3c13a8d8f59b45145b546ec31b5c85d5053e1279fc558c5a1e";
-          uidMaps = ["0:200000:65536"];
-          gidMaps = ["0:200000:65536"];
           # dropCapabilities = ["ALL"];
           # addCapabilities = ["FOWNER" "NET_RAW"];
-          noNewPrivileges = true;
+          # noNewPrivileges = true;
           # readOnly = true;
           # tmpfses = [ "/var/run" "/tmp" ];
-          networks = [ "ha-bridge:ip=192.168.1.4" ];
           devices = [ "/dev/serial/by-id/usb-Silicon_Labs_CP2102N_USB_to_UART_Bridge_Controller_e015830c1ba4eb11a4f62a259da30875-if00-port0:/dev/zwave" ];
           environments.TZ = config.time.timeZone;
         };
       };
     };
-    */
   };
 }
--- sys/hass/netns/delete.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-ip netns del ha
