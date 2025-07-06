9cb794a0daa8b6761da8c43f79b483ad7332b6ea
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2025-07-05 23:37:38 -0400

    asdf

--- sys/hass/home-automation.nix
+++ sys/hass/home-automation.nix
@@ -26,7 +26,8 @@
         containerConfig = {
           name = "nginx";
           pod = "ha.pod";
-          image = "docker.io/library/nginx:alpine-slim@sha256:e4e764cb35f666f44dd4e1da4291a5f73bb8bff2a9464ccecd8a05a2b7226ad5";
+          image =
+            "docker.io/nginxinc/nginx-unprivileged:alpine-slim@sha256:ca2305d71219043ad4cdf91d588b5a4f94d6bc3cd44bfd8667cee0b6c121b712";
           dropCapabilities = ["ALL"];
           addCapabilities = [ "CHOWN" "NET_BIND_SERVICE" ];
           noNewPrivileges = true;
