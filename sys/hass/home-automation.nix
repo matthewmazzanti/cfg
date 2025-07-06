d55303d899a734cf3846e7a00f8732aee102d056
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2025-07-05 23:36:11 -0400

    Allow chown

--- sys/hass/home-automation.nix
+++ sys/hass/home-automation.nix
@@ -28,7 +28,7 @@
           pod = "ha.pod";
           image = "docker.io/library/nginx:alpine-slim@sha256:e4e764cb35f666f44dd4e1da4291a5f73bb8bff2a9464ccecd8a05a2b7226ad5";
           dropCapabilities = ["ALL"];
-          addCapabilities = [ "NET_BIND_SERVICE" ];
+          addCapabilities = [ "CHOWN" "NET_BIND_SERVICE" ];
           noNewPrivileges = true;
           # readOnly = true;
           # tmpfses = [ "/var/run" "/tmp" ];
