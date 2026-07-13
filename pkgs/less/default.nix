c4d9ec491d7b747eae280752b0aeb767ec283dcc
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2026-07-13 08:21:52 -0400

    clean up formatting for less flags

--- pkgs/less/default.nix
+++ pkgs/less/default.nix
@@ -6,22 +6,12 @@ callPackage ./wrapper.nix {
   inherit less;
   lesskey = builtins.readFile ./lesskey;
   wrapperArgs = [
-    "--set"
-    "LESSHISTFILE"
-    "-"
-    "--add-flags"
-    "--chop-long-lines"
-    "--add-flags"
-    "--RAW-CONTROL-CHARS"
-    "--add-flags"
-    "--quit-if-one-screen"
-    "--add-flags"
-    "--mouse"
-    "--add-flags"
-    "--wheel-lines=5"
-    "--add-flags"
-    "--ignore-case"
-    "--add-flags"
-    "--prompt=%lb/%L"
+    "--set" "LESSHISTFILE" "-"
+    "--add-flags" "--chop-long-lines"
+    "--add-flags" "--RAW-CONTROL-CHARS"
+    "--add-flags" "--quit-if-one-screen"
+    "--add-flags" "--mouse"
+    "--add-flags" "--ignore-case"
+    "--add-flags" "--prompt=%lb/%L"
   ];
 }
